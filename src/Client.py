194f99b71912e7758b3be7c5393156994e2687d5
oysteinsigholt <oystein.sigholt@gmail.com>
2015-03-10 10:39:40 +0100

    Added Client class + added bat to .gitignore

---
+++ src/Client.py
@@ -0,0 +1,18 @@
+import Payload
+
+class Client(object):
+	def __init__(self, server_ip, server_port):
+		self.server_ip = server_ip
+		self.server_port = server_port
+
+	def login(self, user):
+		pass #todo
+		
+	def logout():
+		pass #todo
+	
+	def send(payload):
+		pass #todo
+		
+	def receive(payload):
+		pass todo
