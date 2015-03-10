06c26fc88153b69bbe9083d86cf1bf8d669bb738
oysteinsigholt <oystein.sigholt@gmail.com>
2015-03-10 11:00:49 +0100

    Stuff
    Good commenting ettiquette

--- src/Client.py
+++ src/Client.py
@@ -1,18 +1,24 @@
 import Payload
+from socket import *
 
 class Client(object):
 	def __init__(self, server_ip, server_port):
 		self.server_ip = server_ip
 		self.server_port = server_port
+		
+		clientSocket = socket()
+		clientSocket.connect((server_ip, server_port))
+		clientSocket.setblocking(0)
 
 	def login(self, user):
 		pass #todo
 		
-	def logout():
+	def logout(self):
 		pass #todo
 	
-	def send(payload):
+	def send(self, payload):
 		pass #todo
 		
-	def receive(payload):
-		pass todo
+	def receive(self, payload):
+		
+		pass #todo
--- src/Payload.py
+++ src/Payload.py
@@ -1,9 +1,19 @@
+import json
+
 class Payload(object):
-	def __init__(self, type, sender, content):
+	def __init__(self, type=None, sender=None, content=None):
 		self.type = type #string
 		self.sender = sender #User
 		self.content = content #string
 		self.timestamp = 0 #todo
+		
+	def parseJson(self, json_string):
+		jdata = json.loads(json_string)
+		#todo: catch undefined
+		self.type = jdata['type']
+		self.sender = jdata['sender']
+		self.content = jdata['content']
+		self.timestamp = jdata['timestamp']
 
 	def getType(self):
 		return self.type
